e58de7bb0f2ae0941a14b941704be2ef44fe75d9
Andrés Gomez <gomeandr@gmail.com>
2020-02-21 21:01:57 +0100

    translation

--- src/components/Portofolio.vue
+++ src/components/Portofolio.vue
@@ -13,7 +13,9 @@
                         <div class="card">
                             <header class="card-header">
                                 <p class="card-header-title">
-                                    <span>{{ portfolio.titel }} </span>
+                                    <span v-if="$t('lang.current') === 'fr'">{{ portfolio.titel_fr }}</span>
+                                    <span v-else-if="$t('lang.current') === 'en'">{{ portfolio.titel_en }}</span>
+                                    <span v-else>{{ portfolio.titel }} </span>
                                     <span class="is-pulled-right">
                                         <span v-for="tag in portfolio.tags" :key="tag.id" class="tag is-default"> {{ tag }}</span>
                                     </span>
@@ -21,7 +23,7 @@
                             </header>
                             <div class="card-content">
                                 <figure class="image is-2by1">
-                                    <img v-bind:src="portfolio.img" v-bind:alt="portfolio.titel" />
+                                    <img v-bind:src="portfolio.img" v-bind:alt="portfolio.titel_en" />
                                 </figure>
                             </div>
                             <footer v-if="portfolio.source" class="card-footer">
@@ -41,13 +43,15 @@ export default {
     data() {
         return {
             portfolios: [{
-                    titel: 'E-commerce Page',
+                    titel_en: 'E-commerce Page',
+                    titel_fr: 'Page E-commerce',
                     href: 'https://drakar.ch',
                     img: '/ressources/images/2020_drakar.PNG',
                     tags: ['2020', 'Wordpress', 'E-commerce']
                 },
                 {
-                    titel: 'Personal Page ',
+                    titel_en: 'Personal Page ',
+                    titel_fr: 'Page personnelle ',
                     href: 'https://gomeandr.ch',
                     img: '/ressources/images/2020_pagePerso.PNG',
                     source: 'https://github.com/gomeandr/PagePerso',
@@ -61,20 +65,23 @@ export default {
                     tags: ['2018', 'Angular v2', 'Bootstrap']
                 },
                 {
-                    titel: 'Game for TCS',
+                    titel_fr: 'Jeux pédagogique (Touring Club Suisse)',
+                    titel_en: 'Game for Touring Club Switzerland',
                     href: 'https://gomeandr.ch/panne/',
                     img: '/ressources/images/panne.PNG',
                     tags: ['2017', 'Twine']
                 },
                 {
-                    titel: 'Search Engine for Companies',
+                    titel_en: 'Search Engine for Companies',
+                    titel_fr: 'Petit moteur de recherche',
                     href: 'https://gomeandr.ch/search-engine/',
                     img: '/ressources/images/searchTool.PNG',
                     source: 'https://github.com/gomeandr/search-tool',
                     tags: ['2016', 'Angular v1', 'Bootstrap']
                 },
                 {
-                    titel: 'Personal page university',
+                    titel_en: 'Personal page university',
+                    titel_fr: 'Page personnelle de l\'université',
                     href: 'https://tecfaetu.unige.ch/perso/maltt/andresg0/',
                     img: '/ressources/images/2012_Unige_pagePerso.PNG',
                     tags: ['2012', 'HMLT', 'Bootstrap']
